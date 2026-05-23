4311422de7acdf97ffb81f1817393ba404036d42
mehta-y <mehtayuvraj700@gmail.com>
2026-05-23 11:36:00 +0000

    01_basics

---
+++ javascript/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID = "4567890"
+let accountEmail = "mehtayuvraj2202@gmail.com"
+var accountpass = "4567"
+accountcity = "Patna"
+let accountstate;
+
+// accountID = 2     is not allowed beecause of const
+
+accountEmail = "mehtayuvraj700@gmail.com"
+accountpass = "1234"
+accountcity = "BHopal"
+console.log(accountID)
+console.table([accountID,accountEmail,accountpass,accountcity,accountstate])
