3ef877db2e4de6e032da681ad998db51b8ac4b1d
mehta-y <mehtayuvraj700@gmail.com>
2026-05-23 13:00:31 +0000

    datatypes

---
+++ javascript/01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+//  alert(3+3)    we're using node not web browser
+
+let name = "yuvraj"
+let age = 20
+let iseligible = true
+
+
+// number , string , boolean , bigint , null(khali) , undefined , symbol 
+
+
+
+
+// object
+
+console.table([typeof name,typeof age,typeof iseligible])
