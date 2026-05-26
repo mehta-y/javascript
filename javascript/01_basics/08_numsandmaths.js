cb0bb9983624adda9ff03b42887cde742d23753d
mehta-y <mehtayuvraj700@gmail.com>
2026-05-26 11:54:33 +0000

    2

---
+++ javascript/01_basics/08_numsandmaths.js
@@ -0,0 +1,7 @@
+const score = 400
+const balance  = new Number(100)
+console.log(score)              // 400
+console.log(balance)            // [Number:100]
+
+console.log(bbalance.toString)
+console.log(typeof balance)
